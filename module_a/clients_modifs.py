87c5a30e4261402c0b4e5345348937099a55dc57
Christophe Gusthiot <i2g@gusthiot.ch>
2024-03-22 20:34:26 +0100

    - clients modifs, pas si aucun, bonne version

--- module_a/clients_modifs.py
+++ module_a/clients_modifs.py
@@ -23,31 +23,33 @@ class ClientsModifs(CsvDict):
 
         for code in versions.clients_changes:
             if code in self.imports.clients_prev.donnees.keys():
-                self._ajout(self.imports.clients_prev.donnees[code], unique)
+                self._ajout(self.imports.clients_prev.donnees[code], unique, imports.resultats.vfact)
             else:
-                self._ajout_nul(code, unique)
+                self._ajout_nul(code, unique, imports.resultats.vfact)
             unique += 1
             if code in self.imports.clients.donnees.keys():
-                self._ajout(self.imports.clients.donnees[code], unique)
+                self._ajout(self.imports.clients.donnees[code], unique, imports.version)
             else:
-                self._ajout_nul(code, unique)
+                self._ajout_nul(code, unique, imports.version)
             unique += 1
 
-    def _ajout(self, client, unique):
+    def _ajout(self, client, unique, version):
         """
         ajout d'une ligne pour un client
         :param client: données du client
         :param unique: clé unique de la ligne
+        :param version: version de facturation
         """
-        self._ajouter_valeur([self.imports.edition.annee, self.imports.edition.mois, self.imports.version,
+        self._ajouter_valeur([self.imports.edition.annee, self.imports.edition.mois, version,
                               client['code'], client['code_sap'], client['abrev_labo'], client['nom2'], client['nom3'],
                               client['ref'], client['email'], client['mode'], client['id_classe']], unique)
 
-    def _ajout_nul(self, code, unique):
+    def _ajout_nul(self, code, unique, version):
         """
         ajout d'une ligne pour un client non-existant
         :param code: code client
         :param unique: clé unique de la ligne
+        :param version: version de facturation
         """
-        self._ajouter_valeur([self.imports.edition.annee, self.imports.edition.mois, self.imports.version, code,
+        self._ajouter_valeur([self.imports.edition.annee, self.imports.edition.mois, version, code,
                               "", "", "", "", "", "", "", ""], unique)
